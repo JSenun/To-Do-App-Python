8a8064bf3c6d065492a137c81285f889b3f64b82
JSenun KX <joaovictor31415@gmail.com>
2023-03-27 13:19:35 -0300

    Fazendo alterações no notebook jupyter

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+# Criando funções para ler e atualizar o arquivo 'db.json'
 
 def le_arquivo_json(nome_arquivo):
     with open(nome_arquivo, 'r', encoding='utf-8') as arquivo_json:
@@ -12,5 +13,5 @@ def atualiza_arquivo_json(nome_arquivo, dados_processados_arquivo):
         dados = json.loads(dados)
         json.dump(dados, arquivo_json)
         
+# Criando a variavel que vai conter os dados das tarefas
 tarefas_db = le_arquivo_json('db.json')
-print(tarefas_db['tarefas']['tarefas_pendentes'][1])
